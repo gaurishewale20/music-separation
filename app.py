de89e41b66a6555dad1f637526595e6ecf3778d0
Gauri Shewale <gaurishewale201201@gmail.com>
2022-11-24 12:48:56 +0530

    Working prototype of Music-Separation model-website done

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, request, render_template, send_from_directory
 import pandas as pd
 from werkzeug.utils import secure_filename
-import joblib
+# import joblib
 import os
+# import nussl
+import torch
+from openunmix import predict
+import stempeg
+# import musdb
+import librosa
+import torchaudio
+import numpy as np
+import soundfile as sf
+from scipy.io.wavfile import write
+import warnings
+warnings.filterwarnings(action= 'ignore')
+import pydub
 
 # Declare a Flask app
 app = Flask(__name__,template_folder='template')
@@ -28,6 +41,7 @@ def main():
     if request.method == "POST":
 
         uploadedfile = request.files['audiovar']
+        print(uploadedfile.stream)
         if uploadedfile.filename:
             filename = secure_filename(uploadedfile.filename)
             file_ext = os.path.splitext(filename)[1]
@@ -36,14 +50,42 @@ def main():
                 return "<h1> Invalid file type. <h1> <h3><a href='/'> Try again </a></h3>", 400
             uploadedfile.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
             print(uploadedfile.filename)
-         # Unpickle Model
-        # clf = joblib.load("clf.pkl")
 
-        # Put input into suitable format
+            audio, samplerate = stempeg.read_stems(
+                os.path.join(app.config['UPLOAD_FOLDER'],filename),
+                start=0,
+                duration=10,
+                sample_rate=44100.0,
+                dtype=np.float32
+            )
+            estimates = predict.separate(
+                torch.as_tensor(audio).float(),
+                rate=samplerate,
+                targets=['vocals','drums', 'bass', 'other'],
+                residual=True,
+                device='cpu',
+            )
+            sample_rate = int(samplerate)
+            residual  = np.sum([audio.detach().cpu().numpy()[0] for target, audio in estimates.items() if not target=='vocals'],axis=0)
+            vocals = estimates['vocals'].detach().cpu().numpy()[0]
 
-        # Get prediction
-        # prediction = clf.predict(X)
-            return render_template('website.html', filename=filename, vocalsfilename= filename, drumsfilename=filename, bassfilename=filename, otherfilename=filename)
+            print("Residual type: ",type(residual),"Shape: ", residual.shape)
+            print("vocals type: ",type(vocals), "Shape: ", vocals.shape)
+
+            r_targetPath = os.path.join(app.config['UPLOAD_FOLDER'], "residual.mp3")
+            v_targetPath = os.path.join(app.config['UPLOAD_FOLDER'], "vocals.mp3")
+
+            r1 = np.float16(residual.transpose())
+            v1 = np.float16(vocals.transpose())
+
+            # data length must be a multiple of '(sample_width * channels)
+            song = pydub.AudioSegment(r1.tobytes(), frame_rate=sample_rate, sample_width=2, channels=2)
+            song.export(r_targetPath, format="mp3", bitrate="320k")
+
+            song = pydub.AudioSegment(v1.tobytes(), frame_rate=sample_rate, sample_width=2, channels=2)
+            song.export(v_targetPath, format="mp3", bitrate="320k")
+
+            return render_template('website.html', filename=filename , vocals = "vocals.mp3", residual = "residual.mp3")
     else:
         filename = ""
 
@@ -51,7 +93,7 @@ def main():
 
 @app.route('/uploads/<filename>')
 def uploaded_song(filename):
-	return send_from_directory(app.config['UPLOAD_FOLDER'],filename, as_attachment=True)
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename, as_attachment=True)
 
 # Running the app
 if __name__ == '__main__':
